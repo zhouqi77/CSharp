0964d8bcbe3fb277f3695a5680a229768477957b
zhouqi77 <2535438290@qq.com>
2018-11-05 18:48:35 +0800

    v1

---
+++ test3/tests3/tests3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadWord rw = new ReadWord();
+            //测试readWord：
+            //rw.readList2();
+            //测试LCS：
+            //LCS<string> strLCS = new LCS<string>(strList1, strList2);
+            //strLCS.Demo();
+            //string[] str = rw.readList1().ToArray();
+            //string[] str1 = rw.readList2().ToArray();
+
+            bool a = true;
+            while (a)
+            {
+                Console.WriteLine("                                              ");
+                Console.WriteLine("=================选择1-2-3-4==================");
+                Console.WriteLine("**********************************************");
+                Console.WriteLine("* 1: 输入国考_原题.docx；国考_标准答案1.docx *");
+                Console.WriteLine("* 2: 输入国考_原题.docx；国考_标准答案2.docx *");
+                Console.WriteLine("* 3: 输入国考_原题.docx；国考_标准答案3.docx *");
+                Console.WriteLine("* 4: 输入国考_原题.docx；国考_原题.docx      *");
+                Console.WriteLine("**********************************************");
+                Console.WriteLine("input:");
+                string s1 = Console.ReadLine();
+                if (s1.Equals("1"))
+                {
+                    LCS<string> strLCS = new LCS<string>(rw.readList(), rw.readList1());
+                    strLCS.Demo();
+                }
+                else if (s1.Equals("2"))
+                {
+                    LCS<string> strLCS = new LCS<string>(rw.readList(), rw.readList2());
+                    strLCS.Demo();
+                }
+                else if (s1.Equals("3"))
+                {
+                    LCS<string> strLCS = new LCS<string>(rw.readList(), rw.readList3());
+                    strLCS.Demo();
+                }
+                else if (s1.Equals("4"))
+                {
+                    LCS<string> strLCS = new LCS<string>(rw.readList(), rw.readList4());
+                    strLCS.Demo();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong!!!It's:{0}", s1);
+                    Console.WriteLine("Input Any Key Out");
+                    a = false;
+
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
